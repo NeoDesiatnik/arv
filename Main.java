4932dce729bef6584a9a6418511b0e8ae2d3bf2c
NeoDesiatnik <neobackstrom.desiatnik@elev.ga.ntig.se>
2023-12-18 14:51:27 +0100

    Början på arv

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    Spel fortnite = new Spel("fortnite", "shooter", true);
+
+}
---
+++ Spel.java
@@ -0,0 +1,9 @@
+public class Spel {
+    public Spel(String i, String i1, boolean b) {
+        String namn = "";
+        String genre = "";
+        boolean gratis = true;
+    }
+
+
+}
